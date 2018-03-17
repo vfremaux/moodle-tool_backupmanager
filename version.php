eebbfdca093a7fc5894616349902fd7f5ee8749d
valery <valery.fremaux@gmail.com>
2018-03-17 14:44:13 +0100

    change travis versin

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2015042300;
-$plugin->requires = 2017050500;
+$plugin->requires = 2017110800;
 $plugin->component = 'tool_backupmanager';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0 (Build 2015042300)';
+$plugin->release = '3.4.0 (Build 2015042300)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.3.0001';
+$plugin->codeincrement = '3.4.0001';
